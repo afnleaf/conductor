d0b23f9d1dfa504fb8a6b39ed9797ce607da61c2
afnleaf <afnleafow@gmail.com>
2024-07-26 01:16:37 -0400

    Thinking about using this more or having fun in C

--- conductor.py
+++ conductor.py
@@ -7,20 +7,36 @@ port = 9000
 
 print(os.getcwd())
 
+# add routes here
+
+# paleomap3d
 @app.route("/paleomap3d", methods=["POST"])
 def webhook():
     if request.method == "POST":
         if request.json["ref"] == "refs/heads/main":
             #subprocess.call(["~/prod/paleomap3d/deployprod.sh"], shell=True)
             subprocess.run("/bin/bash /home/x230/prod/paleomap3d/deployprod.sh", shell=True)
-            return "Deployment triggered", 200
+            return "Deployment triggered on paleomap3d", 200
         else:
             return "Non main branch", 200
         return "OK", 200
     else:
         return "NO", 200
 
-# add other routes here
+# Pharahobs Bot
+@app.route("/replaycode", methods=["POST"])
+def webhook():
+    if request.method == "POST":
+        if request.json["ref"] == "refs/heads/main":
+            #subprocess.call(["~/prod/paleomap3d/deployprod.sh"], shell=True)
+            subprocess.run("", shell=True)
+            return "Deployment triggered on replaycode-ocr", 200
+        else:
+            return "Non main branch", 200
+        return "OK", 200
+    else:
+        return "NO", 200
+
 
 if __name__ == "__main__":
     print(f"listening on port {port}")
